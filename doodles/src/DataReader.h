538bf4589d4a902a7fb5c0a3850c74cf84d514d7
Ilya <ilya.katsnelson@archer.com>
2022-12-20 17:08:11 -0800

    Fix compile errors, hopefully

--- doodles/src/DataReader.h
+++ doodles/src/DataReader.h
@@ -1,19 +1,24 @@
+//
+//
+//
 
-//#include "StatisticsData.h"
+// #include "StatisticsData.h"
 
 class DataReader {
  public:
 
-  explicit DataReader(const std::string& topic,
-                      const std::string& library_name)
-      : topic_(topic),
+  explicit DataReader(const std::string & name,
+                        const std::string & topic,
+                        const std::string & library_name)
+      : name_{name},
+        topic_(topic),
         library_name_(library_name) {
   }
 
  private:
-  const std::string name_;
-  const std::string topic_;
-  const std::string library_name_;
+  const std::string name_{};
+  const std::string topic_{};
+  const std::string library_name_{};
 
   // Mode Flags
   //bool enable_listener_ = true;
